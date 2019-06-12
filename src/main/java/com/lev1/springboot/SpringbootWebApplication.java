a05a07b46f4f31642c973fa49ba93e86b82a00d0
Lev1MT <14108426@hdu.edu.cn>
2019-06-12 22:26:18 +0800

    修改springboot主程序类，增加@Component注解

--- src/main/java/com/lev1/springboot/SpringbootWebApplication.java
+++ src/main/java/com/lev1/springboot/SpringbootWebApplication.java
@@ -2,8 +2,10 @@ package com.lev1.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = {"com.lev1"})
 public class SpringbootWebApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/lev1/web/controller/HelloController.java
+++ src/main/java/com/lev1/web/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.lev1.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
 
     @RequestMapping("/hello")
