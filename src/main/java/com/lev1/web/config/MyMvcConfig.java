5745030c9cb58aefedf119342928ac7703c8e663
Lev1MT <14108426@hdu.edu.cn>
2019-06-15 00:06:50 +0800

    add static files and templates

---
+++ src/main/java/com/lev1/web/config/MyMvcConfig.java
@@ -0,0 +1,13 @@
+package com.lev1.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+// 使用WebMvcConfigurationSupport
+//@EnableWebMvc  // 此注解代表全面接管SpringMVC，
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurationSupport {
+
+    
+
+}
